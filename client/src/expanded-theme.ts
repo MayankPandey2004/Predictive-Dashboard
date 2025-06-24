ea4219de01e58694263a449f0a621d90737d749e
MayankPandey2004 <mpandey2004@gmail.com>
2025-06-24 18:53:24 +0530

    second commit

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { Palette, PaletteColor } from "@mui/material";
-
 declare module "@mui/material" {
     interface PaletteColor {
         [key: number]: string;
