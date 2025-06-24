35ce5a231981e5b23d735a0368b7ecf2be5384bd
MayankPandey2004 <mpandey2004@gmail.com>
2025-06-24 19:26:48 +0530

    changed structoring

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             <FlexBetween gap="0.75rem">
                 <PixIcon sx={{fontSize: "28px"}}/>
                 <Typography variant='h4' fontSize="16px">
-                    FinSight
+                    FinSight <span style={{color: palette.grey[800], fontSize: "12px"}}>-Mayank Pandey</span>
                 </Typography>
             </FlexBetween>
 
