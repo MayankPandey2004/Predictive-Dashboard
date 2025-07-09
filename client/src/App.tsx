0d7c7e648a88545efe7134b8a17862c785bcec8a
MayankPandey2004 <mpandey2004@gmail.com>
2025-07-09 16:41:44 +0530

    Changed themes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,19 +5,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./scenes/navbar";
 import Dashboard from "./scenes/dashboard";
 import Predictions from "./scenes/predictions"
+import SplashScreen from "./components/SplashScreen";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
       <BrowserRouter>
+        <SplashScreen />
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            <Navbar/>
-            <Routes> 
-              <Route path="/" element={<Dashboard/>} />
-              <Route path="/predictions" element={<Predictions/>} />
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>
