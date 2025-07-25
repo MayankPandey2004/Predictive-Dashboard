e59e948ccaebc7a0ce33a0718360148ab5383a05
MayankPandey2004 <mpandey2004@gmail.com>
2025-07-26 03:16:44 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -24,11 +24,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type"]
-}));
+app.use(cors);
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
