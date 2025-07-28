14405cbce045c4e54578241e940b58a66e6e4535
MayankPandey2004 <mpandey2004@gmail.com>
2025-07-28 22:25:58 +0530

    fixed RTK queries

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -19,13 +19,13 @@ export const api = createApi({
             query: () => "transaction/transactions/",
             providesTags: ["Transactions"]
         }),
-        suggestPrice: build.mutation<{ suggested_price: number; predicted_sales: number; expected_revenue: number }, { price: number; expense: number; sales_volume: number }[]>({
-            query: (body) => ({
-                url: import.meta.env.ML_BASE_URL,
-                method: "POST",
-                body,
-            }),
-        }),
+        // suggestPrice: build.mutation<{ suggested_price: number; predicted_sales: number; expected_revenue: number }, { price: number; expense: number; sales_volume: number }[]>({
+        //     query: (body) => ({
+        //         url: import.meta.env.ML_BASE_URL,
+        //         method: "POST",
+        //         body,
+        //     }),
+        // }),
     })
 })
 
@@ -33,5 +33,4 @@ export const {
     useGetKpisQuery,
     useGetProductsQuery,
     useGetTransactionsQuery,
-    useSuggestPriceMutation
 } = api; 
